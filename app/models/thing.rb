122a9ae8e6a225e878f4b3ac87bd0c410a7682a2
sebabouche <s.nicolaidis@me.com>
2015-12-19 22:51:13 +0100

    Add Thing::Create operation and a first test

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,3 @@
 class Thing < ActiveRecord::Base
+  has_many :comments
 end
---
+++ test/concepts/thing/operation_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ThingOperationTest < MiniTest::Spec
+  describe "Create" do
+    it "persists valid" do
+      thing = Thing::Create.(thing: {name: "Rails", description: "Kickass web dev"}).model
+
+      thing.persisted?.must_equal true
+      thing.name.must_equal "Rails"
+      thing.description.must_equal "Kickass web dev"
+    end
+
+  end
+end
