926391b0010a1626f3f43101bce934173711e747
sebabouche <s.nicolaidis@me.com>
2015-12-27 16:50:02 +0100

    Flash message and redirection

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,11 @@ class CommentsController < ApplicationController
 
   def create
     run Comment::Create do |op|
-      return redirect_to thing_path(op.model.thing)
+      flash[:notice] = "Created comment for \"#{op.thing.name}"
+      return redirect_to thing_path(op.thing)
     end
+
+    @thing = Thing.find(params[:thing_id])
     render action: :new
   end
 end
