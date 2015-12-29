9d34e47ff3d04e158f55efde2f5584a0ffef5c9d
sebabouche <s.nicolaidis@me.com>
2015-12-29 09:45:38 +0100

    On Controller Structuring

--- app/concepts/comment/cell.rb
+++ app/concepts/comment/cell.rb
@@ -22,9 +22,14 @@ class Comment::Cell < Cell::Concept
     inherit_views Comment::Cell
     
     include Kaminari::Cells
+    include ActionView::Helpers::JavaScriptHelper
 
     def show
       render :grid
+    end
+
+    def append
+      %{ $('#next').replaceWith("#{j(show)}") }
     end
 
     private
