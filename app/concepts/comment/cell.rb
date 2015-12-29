b7726852a6f60a228916e2da8f6bc86b785af2a9
sebabouche <s.nicolaidis@me.com>
2015-12-29 02:55:49 +0100

    Kaminari Pagination and Cells

--- app/concepts/comment/cell.rb
+++ app/concepts/comment/cell.rb
@@ -17,4 +17,22 @@ class Comment::Cell < Cell::Concept
   def nice?
     model.weight == 0
   end
+  
+  class Grid < Cell::Concept
+    include Kaminari::Cells
+
+    def show
+      concept("comment/cell", collection: comments) + paginate(comments)
+    end
+
+    private
+
+    def comments
+      @comments ||= model.comments.page(page).per(3)
+    end
+
+    def page
+      options[:page] or 1
+    end
+  end
 end
