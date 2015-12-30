42439da9b2ce4539b108c5563a8568bfccf3681f
sebabouche <s.nicolaidis@me.com>
2015-12-30 11:34:46 +0100

    Adding authorships and has_many through relationships

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,6 +1,7 @@
 class Thing < ActiveRecord::Base
   has_many :comments
-  has_and_belongs_to_many :users
+  has_many :authorships
+  has_many :users, through: :authorships
 
   scope :latest, -> { all.limit(9).order("id DESC") }
 end
