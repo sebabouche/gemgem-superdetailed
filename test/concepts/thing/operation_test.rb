c5ba87c4e714a0d87fdbaef904f212db62b7213c
sebabouche <s.nicolaidis@me.com>
2015-12-30 18:22:12 +0100

    Testing Thing::Create with users

--- test/concepts/thing/operation_test.rb
+++ test/concepts/thing/operation_test.rb
@@ -23,6 +23,64 @@ class ThingOperationTest < MiniTest::Spec
 
       res.must_equal false 
       op.contract.errors.to_s.must_equal "{:description=>[\"is too short (minimum is 4 characters)\"]}" 
+
+    end
+
+    # users
+    it "invalid email" do
+      res, op = Thing::Create.run(thing: {name: "Rails", users: [{"email"=>"invalid format"}, {"email"=>"bla"}]})
+
+      res.must_equal false
+      op.errors.to_s.must_equal "{:\"users.email\"=>[\"is invalid\"]}"
+
+      # still 3 users
+      form = op.contract
+      form.prepopulate! # FIXME: hate this. move prepopulate! to Op#run.
+
+      form.users.size.must_equal 3 # always offer 3 user emails.
+      form.users[0].email.must_equal "invalid format"
+      form.users[1].email.must_equal "bla"
+      form.users[2].email.must_equal nil # this comes from prepopulate!
+    end
+    
+    # all emails blank
+    it "all emails blank" do
+      res, op = Thing::Create.run(thing: {name: "Rails", users: [{"email"=>""}]})
+
+      res.must_equal true
+      op.model.users.must_equal []
+    end
+
+    it "valid, new and existing email" do
+      solnic = User.create(email: "solnic@trb.org") # TODO: replace with operation, once we got one.
+      User.count.must_equal 1
+
+      model = Thing::Create.(thing: {name: "Rails", users: [{"email"=>"solnic@trb.org"}, {"email"=>"nick@trb.org"}]}).model
+
+      model.users.size.must_equal 2
+      model.users[0].attributes.slice("id", "email").must_equal("id"=>solnic.id, "email"=>"solnic@trb.org") # existing user attached to thing.
+      model.users[1].email.must_equal "nick@trb.org" # new user created.
+
+      # authorship is not confirmed, yet.
+      model.authorships.pluck(:confirmed).must_equal [0, 0]
+    end
+
+    # too many users
+    it "users > 3" do
+      emails = 4.times.collect { |i| {"email"=>"#{i}@trb.org"} }
+      res, op = Thing::Create.run(thing: {name: "Rails", users: emails})
+
+      res.must_equal false
+      op.errors.to_s.must_equal "{:users=>[\"is too long (maximum is 3 characters)\"]}"
+    end
+
+    # author has more than 5 unconfirmed authorships.
+    it do
+      5.times { |i| Thing::Create.(thing: {name: "Rails #{i}", users: [{"email"=>"nick@trb.org"}]}) }
+      res, op = Thing::Create.run(thing: {name: "Rails", users: [{"email"=>"nick@trb.org"}]})
+
+      res.must_equal false
+      op.errors.to_s.must_equal "{:\"users.user\"=>[\"This user has too many unconfirmed authorships.\"]}"
     end
   end
 
